970324e27be71fd5daeb7df7484ccd7b47f3183a
Tomasz Ziola <tomek.ziola@gmail.com>
2025-12-19 22:49:18 +0100

    Rename test class from `GithubControllerIntegrationTest` to `RepositoryControllerIntegrationTest` for naming consistency

--- src/test/java/com/ziola/github_repository_retriever/integration/RepositoryControllerIntegrationTest.java
+++ src/test/java/com/ziola/github_repository_retriever/integration/RepositoryControllerIntegrationTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.client.RestTestClient.bindToS
         )
 )
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-class GithubControllerIntegrationTest {
+class RepositoryControllerIntegrationTest {
 
     @LocalServerPort
     private int port;
