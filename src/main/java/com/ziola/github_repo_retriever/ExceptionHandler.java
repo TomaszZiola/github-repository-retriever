b311c6e15661ef8a02364907b4f8374a995c01b5
Tomasz Ziola <tomek.ziola@gmail.com>
2025-12-19 21:26:35 +0100

    Rename classes, methods, and file references for consistency in naming conventions

--- src/main/java/com/ziola/github_repo_retriever/ExceptionHandler.java
+++ src/main/java/com/ziola/github_repo_retriever/ExceptionHandler.java
@@ -1,15 +1,14 @@
 package com.ziola.github_repo_retriever;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
-class GlobalExceptionHandler {
+class ExceptionHandler {
 
-    @ExceptionHandler(UserNotFoundException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(UserNotFoundException.class)
     ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
         var response = new ErrorResponse(NOT_FOUND.value(), ex.getMessage());
         return new ResponseEntity<>(response, NOT_FOUND);
